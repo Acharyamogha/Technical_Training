61f1196e5ad15b93e57647c9104d2259df8bfa6b
Amogha Acharya <140255143+Acharyamogha@users.noreply.github.com>
2024-02-27 19:07:17 +0530

    Add files via upload

---
+++ V_Sem_Programs/sort012.java
@@ -0,0 +1,24 @@
+public class Solution {
+    public void sort012(int[] arr, int n) {
+        int low = 0, high = n - 1, mid = 0;
+        while (mid <= high) {
+            switch (arr[mid]) {
+                case 0:
+                    swap(arr, low++, mid++);
+                    break;
+                case 1:
+                    mid++;
+                    break;
+                case 2:
+                    swap(arr, mid, high--);
+                    break;
+            }
+        }
+    }
+
+    private void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
