ce8018bcb2c8c74eb54137be05cca11eb6ea799a
Amogha Acharya <140255143+Acharyamogha@users.noreply.github.com>
2024-05-26 13:57:09 +0530

    Add files via upload

---
+++ VI_Sem_Programs/RomanToInteger.java
@@ -0,0 +1,36 @@
+public class RomanToInteger {
+    public int romanToInt(String s) {
+        int sum = 0;
+        int prev = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            int current = value(s.charAt(i));
+            if (current < prev) {
+                sum -= current;
+            } else {
+                sum += current;
+            }
+            prev = current;
+        }
+        return sum;
+    }
+
+    private int value(char c) {
+        switch (c) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+            default: return 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        RomanToInteger converter = new RomanToInteger();
+        System.out.println(converter.romanToInt("III")); 
+        System.out.println(converter.romanToInt("LVIII")); 
+        System.out.println(converter.romanToInt("MCMXCIV")); 
+    }
+}
